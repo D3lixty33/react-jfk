76d30cc7e4b11ad2972dd43e656cb684f9e2c0e7
Assistenza <assistenza@itsitalia.local>
2024-09-12 14:32:33 +0200

    Finished adding th eprivacy page, this is gonna be the first deployment version of the website

--- src/components/privacy/privacy-navbar/priv-navbar.js
+++ src/components/privacy/privacy-navbar/priv-navbar.js
@@ -1,8 +1,29 @@
 import React from "react";
+import ITS from '../../../assets/images/Logos/Logo-blu.jpg'
+import './style.css'
+import { Link } from "react-router-dom";
 
 const PrivNavbar = () => {
     return (
-        <div></div>
+        <div className="w-full flex flex-row xxs:flex-col md:flex-row">
+            <div className="flex max-w-32">
+                <img src={ITS} alt="404 Error" className="object-contain"></img>
+            </div>
+            <div className="flex w-full justify-end mr-8 mt-12 text-xl">
+                <ul className="block items-center">
+                    <li className="inline mr-6 liNav">
+                        <Link to={'/'} className="hover:text-sky-500">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="inline mr-6 liNav">About Us</li>
+                    <li className="inline mr-6 liNav">News</li>
+                    <li className="inline">
+                        <button className="text-white btnPrivacy p-1.5">Contact Us</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
--- src/components/privacy/privacy-page.js
+++ src/components/privacy/privacy-page.js
@@ -1,10 +1,76 @@
 import React from "react";
 import PrivNavbar from "./privacy-navbar/priv-navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faEye
+} from '@fortawesome/free-solid-svg-icons';
+import Footer from "../footer/footer";
 
 const PrivacyPage = () => {
     return (
         <div className="w-full">
             <PrivNavbar />
+
+            <div className="flex w-full flex-col text-start mt-8 ml-8">
+                <h1 className="text-5xl font-medium text-[rgb(26,69,104)]">Informative</h1>
+                <div className="mt-8 flex-col flex">
+                    <div className="flex w-full mb-4 mt-4 items-center text-base font-normal  hover:text-sky-500">
+                        <FontAwesomeIcon icon={faEye} />
+                        <p className="ml-2 text-2xl">Policy di gestione dei diritti interessato</p>
+                    </div>
+                    <div>
+                        <hr></hr>
+                    </div>
+                    <div className="flex w-full mb-4 mt-4 items-center text-base font-normal  hover:text-sky-500">
+                        <FontAwesomeIcon icon={faEye} />
+                        <p className="ml-2 text-2xl">Modelli di gestione dei diritti interessato</p>
+                    </div>
+                    <div>
+                        <hr></hr>
+                    </div>
+                    <div className="flex w-full mb-4 mt-4 items-center text-base font-normal  hover:text-sky-500">
+                        <FontAwesomeIcon icon={faEye} />
+                        <p className="ml-2 text-2xl">Informativa privacy generale ex art. 13 del Regolamento (UE) </p>
+                    </div>
+                    <div>
+                        <hr></hr>
+                    </div>
+                    <div className="flex w-full mb-4 mt-4 items-center text-base font-normal  hover:text-sky-500">
+                        <FontAwesomeIcon icon={faEye} />
+                        <p className="ml-2 text-2xl">Privacy Policy e Cookie Policy per i dati raccolti attraverso il sito web</p>
+                    </div>
+                    <div>
+                        <hr></hr>
+                    </div>
+                    <div className="flex w-full mb-4 mt-4 items-center text-base font-normal  hover:text-sky-500">
+                        <FontAwesomeIcon icon={faEye} />
+                        <p className="ml-2 text-2xl">Informativa per invio spontaneo di Curricula personali (viatea, studiorum et laboris)</p>
+                    </div>
+                    <div>
+                        <hr></hr>
+                    </div>
+                    <div className="flex w-full mb-4 mt-4 items-center text-base font-normal  hover:text-sky-500">
+                        <FontAwesomeIcon icon={faEye} />
+                        <p className="ml-2 text-2xl">Informativa per la privacy relativa all'impianto di videosorveglianza ai sensi art.13 Regolamento EU 679/2016</p>
+                    </div>
+                    <div>
+                        <hr></hr>
+                    </div>
+                    <div className="flex w-full mb-4 mt-4 items-center text-base font-normal  hover:text-sky-500">
+                        <FontAwesomeIcon icon={faEye} />
+                        <p className="ml-2 text-2xl">Informativa whistleblowing</p>
+                    </div>
+                    <div>
+                        <hr></hr>
+                    </div>
+                    <div className="flex w-full mb-4 mt-4 items-center text-base font-normal  hover:text-sky-500">
+                        <FontAwesomeIcon icon={faEye} />
+                        <p className="ml-2 text-2xl">Informativa MOGCN-GEN</p>
+                    </div>
+                </div>
+            </div>
+            <div className="h-40"></div>
+            <Footer />
         </div>
     )
 }
